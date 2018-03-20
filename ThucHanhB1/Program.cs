cd0e4eca8bfbcd25d60cf2840aab3a659508e55b
maxukiban <37258248+maxukiban@users.noreply.github.com>
2018-03-20 15:47:29 +0700

    Bai1-2

---
+++ ThucHanhB1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace ThucHanhBuoi1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //bai 1
+            String strHostname = Dns.GetHostName();
+            Console.WriteLine("Host Name: " + strHostname);
+            IPHostEntry iphost = Dns.GetHostEntry(strHostname);
+            int nIp = 0;
+            foreach (IPAddress ipaddress in iphost.AddressList)
+            {
+                Console.WriteLine("IP #" + ++nIp + ": " + ipaddress.ToString());
+            }
+            //bai 2
+            Console.WriteLine("Nhap ten mien hoac nhap ip: ");
+            string host = Console.ReadLine();
+            IPHostEntry e = Dns.GetHostEntry(host);
+            Console.WriteLine("Ten Mien: ", e.HostName);
+            foreach (string s in e.Aliases)
+                Console.WriteLine("Bi danh: " + s);
+            foreach (IPAddress i in e.AddressList)
+                Console.WriteLine("IP: " + i);
+            
+           
+        }
+    }
+}
